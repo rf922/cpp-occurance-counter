c46d52448f5f843304b010129b9b1b37b3cc9d54
rf922 <rfabiani@sfsu.edu>
2024-01-22 15:41:12 -0800

    created function for counting occurances and moved logic out of main. refatored main to implement multithreading using hardware concurrency in thread to detect the number of threads available. split the file into chuncks for each thread to process. planning on eventuaally implementing logic to compare run times between varying number of threads

--- main.cpp
+++ main.cpp
@@ -18,6 +18,11 @@
 #include <optional>
 #include <stdexcept>
 #include <chrono>
+#include <future>
+#include <thread>
+#include <vector>
+
+#define DATA_FILE_PATH "data/threesData.bin"
 
 using namespace std;
 
@@ -45,38 +50,95 @@ std::optional<int32_t> parseArgument(int argc, char** argv) {
     }
     return std::nullopt;
 }
+/**
+ * logic to open and count occurrences of a val within a file and increment
+ * the total count of occurrences 
+ * @param val the value to be search
+ * @param start starting pos in the file
+ * @param end ending pos in the file
+ * @param count total count of times val has been seen
+ */
+void countOccurances(int32_t val, streampos start, streampos end, atomic<int>& count) {
+    //init the input file stream to read from 
+    ifstream dataFile(DATA_FILE_PATH, ios::binary);
 
+    if (!dataFile) {//ensure the file can be opened
+        throw runtime_error("threesData.bin could not be opened!");
+    }
+    
+    dataFile.seekg(start); // move to starting pos in the file
+    int32_t dataValue; //init val to store value from file
+    
+    // while the current pos has not reached the end and vals can be read 
+    while (dataFile.tellg() < end && dataFile.read(reinterpret_cast<char *> (&dataValue), sizeof (dataValue))) {
+        //compare and increment the count as maches are seen
+        if (dataValue == val) {
+            count++;
+        }
+    }
+
+}
+
+/**
+ * main entry point for the program. Verifies that the file can be opened
+ * then determines a value for the number of threads to be used in the program.
+ * The program then reads in the size of the file and computes an appropriate chunck
+ * size to pass to each thread. Once the program completes the 
+ * @param argc
+ * @param argv
+ * @return 
+ */
 int main(int argc, char** argv) {
     try {
+        // Init the timer and val to be searched for
         auto targetValue = parseArgument(argc, argv).value_or(3);
-        
-        // Init timer 
         auto startTime = chrono::steady_clock::now();
 
-        //init the count to 0 and a temp container for value read
-        int count = 0;
-        int32_t dataValue;
-        
-        //init the input file stream to read from 
-        ifstream dataFile("data/threesData.bin", ios::binary);
-        
+        // detect machines hardware to set an num threads to use
+        unsigned int numThreads = thread::hardware_concurrency();
+        if (numThreads == 0) {
+            numThreads = 1; // Default fallback for numthreads
+        }
+
+        // verify that the file can be opened 
+        ifstream dataFile(DATA_FILE_PATH, ios::binary | ios::ate);
         if (!dataFile) {
             throw runtime_error("threesData.bin could not be opened!");
         }
 
-        while (dataFile.read(reinterpret_cast<char *> (&dataValue), sizeof (dataValue))) {
-            if (dataValue == targetValue) {
-                count++;
-            }
-        }
+        // determine the files total size
+        streampos fileSize = dataFile.tellg();
+        dataFile.close();
+
         
+        
+        atomic<int> totalCount = 0;
+        vector<thread> threads;
+        
+        //compute appropriate chunck size for each thread to process
+        streampos chunckSize = fileSize / numThreads;
+
+        // init each thread 
+        for (unsigned int i = 0; i < numThreads; ++i) {
+            
+            //compute the correct start and end pos
+            streampos start = i * chunckSize;
+            streampos end = (i == numThreads - 1) ? fileSize : static_cast<streampos> ((i + 1) * chunckSize);
+            // create and add each thread giving the func to execute, with its proper args
+            threads.emplace_back(countOccurances, targetValue, start, end, ref(totalCount));
+        }
+
+        for (auto& t : threads) { //join the threads 
+            t.join();
+        }
+
         // end time marker
         auto endTime = chrono::steady_clock::now();
-        cout << "Number of occurrences of '" << targetValue << "': " << count << endl;
-        
+        cout << "Number of occurrences of '" << targetValue << "': " << totalCount << endl;
+        cout << "Number of Threads used : " << numThreads << endl;
         chrono::duration<double> elapsed_seconds = endTime - startTime;
         cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";
-        
+
     } catch (const std::exception& e) {
         cerr << "An error occurred: " << e.what() << endl;
         return 1;
